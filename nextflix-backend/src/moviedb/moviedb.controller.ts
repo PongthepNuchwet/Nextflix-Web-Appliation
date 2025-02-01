97d2aa7180e1379e632f03d9aafb40cd654ff99f
PongthepNuchwet <jt061220@gmail.com>
2025-02-01 22:45:26 +0700

    feat: add movie details and images endpoints to MoviedbController

--- nextflix-backend/src/moviedb/moviedb.controller.ts
+++ nextflix-backend/src/moviedb/moviedb.controller.ts
@@ -8,17 +8,38 @@ import {
 import { Movie } from './interfaces/movie.interface';
 import { MoviedbService } from './moviedb.service';
 import { TrendingMediaDto } from './dto/trending-media.dto';
+import { QueryMediaDto } from './dto/detail-media.dto';
 
 @Controller('moviedb')
 export class MoviedbController {
   constructor(private readonly moviesService: MoviedbService) {}
 
   @Get('trending')
-  @UsePipes(new ValidationPipe({ transform: true }))
+  @UsePipes(new ValidationPipe())
   async getTrendingMovies(@Query() query: TrendingMediaDto): Promise<Movie[]> {
     return this.moviesService.getTrendingMovies({
       mediaType: query.mediaType,
       frequency: query.frequency,
+      lang: query.lang || 'en-US',
+    });
+  }
+
+  @Get('details')
+  @UsePipes(new ValidationPipe())
+  async getMovieDetails(@Query() query: QueryMediaDto) {
+    return this.moviesService.getMovieDetails({
+      id: query.id,
+      mediaType: query.mediaType,
+      lang: query.lang || 'en-US',
+    });
+  }
+
+  @Get('images')
+  @UsePipes(new ValidationPipe())
+  async getMovieImages(@Query() query: QueryMediaDto) {
+    return this.moviesService.getMediaImages({
+      id: query.id,
+      mediaType: query.mediaType,
       lang: query.lang || 'en-US',
     });
   }
