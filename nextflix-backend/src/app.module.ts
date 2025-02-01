2a2195bb0cad01e29b1307eb5410357c7b3d7853
PongthepNuchwet <jt061220@gmail.com>
2025-02-01 22:31:06 +0700

    feat: configure global settings and integrate Moviedb module in app module

--- nextflix-backend/src/app.module.ts
+++ nextflix-backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MoviedbModule } from './moviedb/moviedb.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MoviedbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
