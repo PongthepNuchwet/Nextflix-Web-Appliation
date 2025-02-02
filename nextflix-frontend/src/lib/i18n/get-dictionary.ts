114b685a7ea5d049024e10e0022c9eebfb28623a
PongthepNuchwet <jt061220@gmail.com>
2025-02-03 00:19:16 +0700

    feat: update localization dictionaries and add DictionaryType interface for improved i18n support

--- nextflix-frontend/src/lib/i18n/get-dictionary.ts
+++ nextflix-frontend/src/lib/i18n/get-dictionary.ts
@@ -1,10 +1,11 @@
 import 'server-only'
 import { Locale } from './i18n-config'
+import { DictionaryType } from '@/types/dictionaries'
 
 const dictionaries = {
     'en': () => import('@/dictionaries/en.json').then((module) => module.default),
     'th': () => import('@/dictionaries/th.json').then((module) => module.default),
 }
 
-export const getDictionary = async (locale: Locale) =>
+export const getDictionary = async (locale: Locale): Promise<DictionaryType> =>
     dictionaries[locale]()
---
+++ nextflix-frontend/src/types/dictionaries.ts
@@ -0,0 +1,16 @@
+export interface DictionaryType {
+    hello: string
+    no_movies_available: string
+    loading: string
+    error_fetch_movies: string
+    my_list: string
+    play: string
+    info: string
+    more_info: string
+    no_description: string
+    home: string
+    tv_shows: string
+    movies: string
+    new_popular: string
+    browse_by_language: string
+}
