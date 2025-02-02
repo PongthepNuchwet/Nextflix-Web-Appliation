5c622401233727824a93e9177818be1d56d1d361
PongthepNuchwet <jt061220@gmail.com>
2025-02-03 01:19:17 +0700

    feat: update RootLayout to fetch and pass localization dictionary to TapBar

--- nextflix-frontend/src/app/[lang]/layout.tsx
+++ nextflix-frontend/src/app/[lang]/layout.tsx
@@ -1,13 +1,21 @@
-import TapBar from "@/components/layout/TapBar";
+import TapBar from "@/components/layout/TopBar";
+import { getDictionary } from "@/lib/i18n/get-dictionary";
+import { Locale } from "@/lib/i18n/i18n-config";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { lang: Locale };
 }) {
+
+  const lang = params.lang;
+  const dict = await getDictionary(lang);
+
   return (
     <main>
-      <TapBar />
+      <TapBar dict={dict} />
       {children}
     </main>
   );
