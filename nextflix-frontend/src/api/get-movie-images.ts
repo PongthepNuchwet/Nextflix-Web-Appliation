3505eba4684c2febc69ea22aa1c35656dfd8b369
PongthepNuchwet <jt061220@gmail.com>
2025-02-03 01:18:01 +0700

    feat: add API functions for fetching movie images and trending movies

---
+++ nextflix-frontend/src/api/get-movie-images.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { i18n } from "@/lib/i18n/i18n-config";
+import { MovieImages } from "@/types/images-movie.interface";
+
+type Props = {
+    mediaType: 'movie' | 'tv';
+    id: number;
+    lang?: string;
+};
+
+export const getMovieImages = async ({ mediaType, id, lang = i18n.defaultLocale }: Props): Promise<MovieImages | null> => {
+    try {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_HOST}/moviedb/images?mediaType=${mediaType}&id=${id}&lang=${lang}`,
+            {
+                method: 'GET',
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch images: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching movie images:", error);
+        return null;
+    }
+};
---
+++ nextflix-frontend/src/api/get-trending-movies.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { Movie } from "@/types/movie.interface";
+
+type Props = {
+    mediaType: 'movie' | 'tv' | 'all';
+    frequency: 'day' | 'week';
+    lang: string;
+};
+
+export const getTrendingMovies = async ({ mediaType, frequency, lang }: Props): Promise<Movie[] | null> => {
+    try {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_HOST}/moviedb/trending?mediaType=${mediaType}&frequency=${frequency}&lang=${lang}`,
+            {
+                method: 'GET',
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch trending movies: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching trending movies:", error);
+        return null;
+    }
+};
