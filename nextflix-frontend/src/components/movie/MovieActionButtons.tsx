3c64abab003253c6e302c3f2062772532baa9430
PongthepNuchwet <jt061220@gmail.com>
2025-02-03 12:33:24 +0700

    refactor: rename ActionMovieScreen to MovieActionButtons and enhance with dialog for video playback

--- nextflix-frontend/src/components/movie/MovieActionButtons.tsx
+++ nextflix-frontend/src/components/movie/MovieActionButtons.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Button } from "../ui/button";
 import { FaPlay } from "react-icons/fa";
 import { IoMdInformationCircleOutline, IoMdAdd } from "react-icons/io";
 import { DictionaryType } from "@/types/dictionaries";
+import {
+    Dialog,
+    DialogContent,
+    // DialogDescription,
+    // DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import MovieVideo from "./MovieVideo";
+import { Movie } from "@/types/movie.interface";
+import { Locale } from "@/lib/i18n/i18n-config";
 
-type Props = {
-    movieID: number;
+type MovieActionButtonsProps = {
+    movie: Movie
     dict: DictionaryType
+    lang: Locale;
 };
 
-export default function ActionMovieScreen({ movieID, dict }: Props) {
+export default function MovieActionButtons({ movie, dict, lang }: MovieActionButtonsProps) {
     return (
         <div className="flex flex-row items-center justify-evenly md:items-start md:justify-start gap-4 w-full">
 
@@ -20,10 +32,22 @@ export default function ActionMovieScreen({ movieID, dict }: Props) {
             </Button>
 
             {/* Play Button */}
-            <Button variant="default" className="bg-white text-black font-medium text-lg flex items-center gap-2">
-                <FaPlay size={20} />
-                {dict.play}
-            </Button>
+            <Dialog >
+                <DialogTrigger asChild>
+                    <Button variant="default" className="bg-white text-black font-medium text-lg flex items-center gap-2">
+                        <FaPlay size={20} />
+                        {dict.play}
+                    </Button>
+                </DialogTrigger>
+                <DialogContent className="bg-white dark:bg-gray-900 lg:min-w-3xl" autoFocus={false}>
+                    < DialogTitle className="text-black dark:text-white">
+                        {movie.title || movie.name}
+                    </DialogTitle>
+                    <Suspense fallback={<div>{dict.loading}</div>}>
+                        <MovieVideo lang={lang} movie={movie} />
+                    </Suspense>
+                </DialogContent>
+            </Dialog>
 
             {/* Info Button (Mobile) */}
             <Button variant="ghost" className="md:hidden text-white font-medium text-lg p-0 flex flex-col gap-0 h-auto">
