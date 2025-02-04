8c0d4f1f1d8a634fcfe8397bd83d7bb28c396c24
PongthepNuchwet <jt061220@gmail.com>
2025-02-05 01:44:44 +0700

    feat: refactor MovieVideo component to accept media type and movie ID as props; improve error handling and add loading state

--- nextflix-frontend/src/components/movie/MovieVideo.tsx
+++ nextflix-frontend/src/components/movie/MovieVideo.tsx
@@ -1,31 +1,39 @@
 import { getOneMovieVideos } from '@/services/fetchMovieVideos';
 import { Locale } from '@/lib/i18n/i18n-config';
-import { Movie } from '@/types/movie.interface';
-import React from 'react'
+import { mediaType } from '@/types/movie.interface';
+
 import NoData from '../NoData';
 import ErrorMessage from '../ErrorMessage';
 import MovieYoutubeEmbed from './MovieYoutubeEmbed';
 
 
 type MovieVideoProps = {
-    movie: Movie
+    mediaType: mediaType
+    movie_id: number
     lang: Locale
 }
 
-export default async function MovieVideo({ movie, lang }: MovieVideoProps) {
+export default async function MovieVideo({ mediaType, lang, movie_id }: MovieVideoProps) {
     try {
-        const video = await getOneMovieVideos({ mediaType: movie.media_type, lang, id: movie.id });
+        const video = await getOneMovieVideos({ mediaType, lang, id: movie_id });
 
         if (!video) {
-            return <NoData message="No " />;
+            return <NoData message="No video available for this movie." />;
         }
 
-        return (
-            <MovieYoutubeEmbed videoId={video.key} />
-        );
-
+        return <MovieYoutubeEmbed videoId={video.key} />;
     } catch (error) {
-        console.error("Error fetching movies:", error);
-        return <ErrorMessage message="" />;
+        console.error("Error fetching movie video:", error);
+        return <ErrorMessage message="Failed to load movie video. Please try again later." />;
     }
+}
+
+
+
+export function MovieVideoLoading() {
+    return (
+        <div className="animate-pulse w-full p-4">
+            <div className="h-30 bg-gray-800 rounded w-full"></div>
+        </div>
+    )
 }
