6b73c2d6dc0e2587c610620b79af85e0426224c0
PongthepNuchwet <jt061220@gmail.com>
2025-02-05 01:44:22 +0700

    feat: update MovieActionButtons to enhance UI with improved button interactions and navigation links

--- nextflix-frontend/src/components/movie/MovieActionButtons.tsx
+++ nextflix-frontend/src/components/movie/MovieActionButtons.tsx
@@ -6,14 +6,14 @@ import { DictionaryType } from "@/types/dictionaries";
 import {
     Dialog,
     DialogContent,
-    // DialogDescription,
-    // DialogHeader,
+    DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
 import MovieVideo from "./MovieVideo";
 import { Movie } from "@/types/movie.interface";
 import { Locale } from "@/lib/i18n/i18n-config";
+import Link from "next/link";
 
 type MovieActionButtonsProps = {
     movie: Movie
@@ -26,7 +26,7 @@ export default function MovieActionButtons({ movie, dict, lang }: MovieActionBut
         <div className="flex flex-row items-center justify-evenly md:items-start md:justify-start gap-4 w-full">
 
             {/* My List (Mobile) */}
-            <Button variant="ghost" className="md:hidden text-white font-medium p-0 text-lg flex flex-col items-center gap-0 h-auto">
+            <Button variant="ghost" className="cursor-pointer md:hidden text-white font-medium p-0 text-lg flex flex-col items-center gap-0 h-auto">
                 <IoMdAdd style={{ width: 30, height: 30 }} />
                 {dict.my_list}
             </Button>
@@ -34,32 +34,30 @@ export default function MovieActionButtons({ movie, dict, lang }: MovieActionBut
             {/* Play Button */}
             <Dialog >
                 <DialogTrigger asChild>
-                    <Button variant="default" className="bg-white text-black font-medium text-lg flex items-center gap-2">
+                    <Button variant="default" className="cursor-pointer  font-medium text-lg flex items-center gap-2">
                         <FaPlay size={20} />
                         {dict.play}
                     </Button>
                 </DialogTrigger>
-                <DialogContent className="bg-white dark:bg-gray-900 lg:min-w-3xl" autoFocus={false}>
-                    < DialogTitle className="text-black dark:text-white">
-                        {movie.title || movie.name}
-                    </DialogTitle>
+                <DialogContent className="bg-white dark:bg-black border-none md:min-w-2xl lg:min-w-4xl p-0 pb-6" autoFocus={false}>
+                    <DialogHeader className="m-0">
+                        < DialogTitle className="text-black dark:text-white p-4">
+                            {movie.title || movie.name}
+                        </DialogTitle>
+                    </DialogHeader>
                     <Suspense fallback={<div>{dict.loading}</div>}>
-                        <MovieVideo lang={lang} movie={movie} />
+                        <MovieVideo lang={lang} movie_id={movie.id} mediaType={movie.media_type} />
                     </Suspense>
                 </DialogContent>
             </Dialog>
 
-            {/* Info Button (Mobile) */}
-            <Button variant="ghost" className="md:hidden text-white font-medium text-lg p-0 flex flex-col gap-0 h-auto">
+            {/* Info Button  */}
+            <Link href={`/${lang}/${movie.media_type == 'movie' ? 'movies' : 'tvshows'}/${movie.id.toString()}`} className=" flex flex-col  md:flex-row gap-0 md:gap-2 md:bg-[#515451] md:rounded-md items-center  cursor-pointer px-4 py-1.5 p-0 font-medium text-white  text-lg  h-auto">
                 <IoMdInformationCircleOutline style={{ width: 30, height: 30 }} />
-                {dict.info}
-            </Button>
+                <span className="md:hidden">{dict.info}</span>
+                <span className=" hidden md:inline-block">{dict.more_info}</span>
+            </Link>
 
-            {/* More Info Button (Desktop) */}
-            <Button variant="default" className="hidden md:flex bg-[#515451] text-white font-medium text-lg items-center gap-2">
-                <IoMdInformationCircleOutline style={{ width: 30, height: 30 }} />
-                {dict.more_info}
-            </Button>
         </div>
     );
 }
