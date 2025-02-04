202bb4063b07caf9e767c2e1e6ad9bc93a9bb054
PongthepNuchwet <jt061220@gmail.com>
2025-02-05 01:39:57 +0700

    feat: enhance ErrorMessage component with optional full-screen display and error icon

--- nextflix-frontend/src/components/ErrorMessage.tsx
+++ nextflix-frontend/src/components/ErrorMessage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { BiSolidError } from "react-icons/bi";
 
 type Props = {
     message: string;
+    fullScreen?: boolean;
 };
 
-export default function ErrorMessage({ message }: Props) {
+export default function ErrorMessage({ message, fullScreen }: Props) {
     return (
-        <div className="flex items-center justify-center h-screen bg-red-100 text-red-700 p-6 rounded-md">
+        <div className={`flex flex-col gap-5 items-center justify-center ${fullScreen ? 'h-screen' : ''}  text-red-700 p-6 rounded-md`}>
+            <BiSolidError size={50} />
             <p className="text-lg font-semibold">{message}</p>
         </div>
     );
